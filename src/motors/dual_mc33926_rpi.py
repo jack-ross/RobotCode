3367bba8f605dabefb74c6eefcace2c34f1c0edd
Jack Ross <jross2112@gmail.com>
2017-04-27 12:57:45 -0400

    Updated for WiringPi pinout

--- src/motors/dual_mc33926_rpi.py
+++ src/motors/dual_mc33926_rpi.py
@@ -12,27 +12,27 @@ def io_init():
         return
 
     wiringpi.wiringPiSetupGpio()
-    wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
-    wiringpi.pinMode(21, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.pinMode(1, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.pinMode(29, wiringpi.GPIO.PWM_OUTPUT)
 
     wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
     wiringpi.pwmSetRange(MAX_SPEED)
     wiringpi.pwmSetClock(2)
 
-    wiringpi.pinMode(20, wiringpi.GPIO.OUTPUT)
+    wiringpi.pinMode(28, wiringpi.GPIO.OUTPUT)
+    wiringpi.pinMode(6, wiringpi.GPIO.OUTPUT)
     wiringpi.pinMode(25, wiringpi.GPIO.OUTPUT)
-    wiringpi.pinMode(26, wiringpi.GPIO.OUTPUT)
 
     # defaults based on page 17 of 
     # https://www.pololu.com/file/download/MC33926.pdf?file_id=0J233
-    wiringpi.pinMode(12, wiringpi.GPIO.OUTPUT)
-    wiringpi.pinMode(16, wiringpi.GPIO.OUTPUT)
-    wiringpi.pinMode(23, wiringpi.GPIO.OUTPUT)
-    wiringpi.pinMode(24, wiringpi.GPIO.OUTPUT)
-    wiringpi.digitalWrite(12, 0) # M2D1
-    wiringpi.digitalWrite(16, 1) # M2nD2
-    wiringpi.digitalWrite(23, 0) # M1D1
-    wiringpi.digitalWrite(24, 1) # M1nD2
+    wiringpi.pinMode(26, wiringpi.GPIO.OUTPUT)
+    wiringpi.pinMode(27, wiringpi.GPIO.OUTPUT)
+    wiringpi.pinMode(4, wiringpi.GPIO.OUTPUT)
+    wiringpi.pinMode(5, wiringpi.GPIO.OUTPUT)
+    wiringpi.digitalWrite(26, 0) # M2D1
+    wiringpi.digitalWrite(27, 1) # M2nD2
+    wiringpi.digitalWrite(4, 0) # M1D1
+    wiringpi.digitalWrite(5, 1) # M1nD2
 
     io_initialized = True
 
@@ -63,9 +63,9 @@ class Motors(object):
     def __init__(self):
         #  pwm_pin(in2), dir_pin(in1)
         io_init()
-        self.motor1 = Motor(18, 26)
-        self.motor2 = Motor(21, 20)
-        self.enable_pin = 25
+        self.motor1 = Motor(1, 25)
+        self.motor2 = Motor(29, 28)
+        self.enable_pin = 6
 
     def enable(self):
         io_init()

78a9a471a891ec2206b372e23ecaa1f6a280f43e
mcr5fh <mcr5fh@virginia.edu>
2017-05-01 01:44:46 -0400

    Shelled out some more threading code

--- src/index.py
+++ src/index.py
@@ -1,36 +1,57 @@
 import logging
 import threading
+#from multiprocessing import Pool
 import time
 import test
 
+import readRotary as encoderControl
+
+from motors.dual_mc33926_rpi import motors, MAX_SPEED
+ 
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)-10s) %(message)s',
                     )
 
+def init_Motors():
+    motors.enable()
+    time.sleep(2)
+    motors.setSpeeds(0, 0)
+
+def init_Encoders():
+
 #Has to be a dict b/c they are mutable
-count = {"leftEncoder":0, "rightEncoder":0}
+encoderCount = {"leftEncoder":0, "rightEncoder":0}
 
 def readEncoder(count):
     logging.debug('Starting Encoder Thread')
-    initPins(19,13)
-    readRotors(encoderCount)
-    logging.debug(test.changeTup(count))
-    #time.sleep(2)
-    #logging.debug('Exiting')
+    encoderControl.initPins(19,13)
+    encoderControl.readRotors(encoderCount)
+    logging.debug(encoderCount)
 
 def motorControl():
+    logging.debug('Starting Motor Thread')
     logging.debug('Starting')
-    time.sleep(3)
+    #PID control
     logging.debug('Exiting')
 
-t = threading.Thread(name='encoders', target=readEncoder, args=(count,))
-# w = threading.Thread(name='motors', target=motorControl)
-#w2 = threading.Thread(target=worker) # use default name
+def mqttControl():
+    logging.debug('Starting mqtt Thread')
+    logging.debug('Starting')
+    #MQTT Polling
+    logging.debug('Exiting')
 
-# w.start()
-# w2.start()
-t.start()
+def 
+init_Motors()
 
+encoderThread = threading.Thread(name='encodersThread', target=readEncoder, args=(count,))
+motorThread = threading.Thread(name='motorsThread', target=motorControl)
+mqttThread = threading.Thread(name='mqttThread', target=mqttControl)
+
+encoderThread.start()
+motorThread.start()
+
+#will this need locks? Encoders writes and the other one reads?
+#
 while True:
 	print count
 	time.sleep(2)

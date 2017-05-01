092adc28f7f1ec669809e3c8375ea4a59f146396
mcr5fh <mcr5fh@virginia.edu>
2017-04-30 20:54:16 -0400

    Minor threading updates

--- src/index.py
+++ src/index.py
@@ -8,9 +8,12 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 
 #Has to be a dict b/c they are mutable
-count = {"c":0}
+count = {"leftEncoder":0, "rightEncoder":0}
+
 def readEncoder(count):
-    logging.debug('Starting')
+    logging.debug('Starting Encoder Thread')
+    initPins(19,13)
+    readRotors(encoderCount)
     logging.debug(test.changeTup(count))
     #time.sleep(2)
     #logging.debug('Exiting')

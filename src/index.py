235884257541a17c8641a438dd418d9565bf9c8b
mcr5fh <mcr5fh@virginia.edu>
2017-05-08 19:44:24 -0400

    start hardcoded

--- src/index.py
+++ src/index.py
@@ -43,7 +43,7 @@ standard_speed = 50
 robot_name = "Robot 1:"
 robot_topic_name = "robot-1"
 
-mqttClient = MQTTClient(robot_name, robot_topic_name, distanceToGoal, angleToGoal, permissionToMove)
+#mqttClient = MQTTClient(robot_name, robot_topic_name, distanceToGoal, angleToGoal, permissionToMove)
 
 
 '''
@@ -181,11 +181,24 @@ def turn(degrees):
 if __name__ == "__main__":
 
     # init_Robot()
-    mqttThread.start()
+    #mqttThread.start()
     motors.enable()
     # distanceToGoal = 10
     # motors.setSpeeds(0, 0)
-    
+    alleyLength = 100 #cm
+    alleyWidth = 50 #cm
+    numAlleys = 3
+
+    for i in xrange(numAlleys):
+        distanceToGoal.value = alleyLength
+        move()
+        turn(90)
+        distanceToGoal.value = alleyWidth
+        move()
+        turn(90)
+        #ready for next alley
+
+
     try:
         while True:
             # print "angle to goal " + str(angleToGoal.value)
